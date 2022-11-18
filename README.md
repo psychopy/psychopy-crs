2cb937521a4067ad0b1473ea4d2fa05ff8f72bc5
mdcutone <matthew.cutone@gmail.com>
2022-11-18 12:22:23 -0500

    NF: Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # psychopy-crs
-Extension to add support for Cambridge Research Systems hardware to PsychoPy.
+
+Extension package for PsychoPy which adds support for various hardware devices by Cambridge Research Systems. 
+
+## Supported Devices
+
+Installing this package alongside PsychoPy will enable support for the following devices:
+
+    * Bits# and Bits++ Stimulus Processors
+    * Display++ and Display++ Touch LCD Monitors
+    * ColorCAL Colorimeters
+    
+## Installing
+
+Install this package with the following shell command:: 
+
+    pip install psychopy-crs
+
+You may also use PsychoPy's builtin package manager to install this package.
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+name = "psychopy-crs"
+version = "0.1.0"
+packages = [
+    'psychopy_crs'
+]
+package_dir = {
+    'psychopy_crs': 'psychopy_crs'
+}
+package_data = {
+   "": ["*.txt", "*.md"]
+}
+
+description = (
+    "Extension package for PsychoPy which adds support for various hardware "
+    "devices by Cambridge Research Systems.")
+
+setup(
+    name=name,
+    version=version,
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    author="Matthew D. Cutone",
+    author_email="mcutone@opensciencetools.org",
+    description=description,
+    url="https://github.com/mdcutone/psychopy-crs",
+    classifiers=[
+        "License :: OSI Approved :: GPL3",
+        'Programming Language :: Python :: 3'],
+    keywords="psychopy hardware photometer display"
+)
