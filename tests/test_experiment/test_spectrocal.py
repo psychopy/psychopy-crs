be89d829ca87697c272f08c4e295203d1f12bcdd
Todd OST <todd.e.parsons@gmail.com>
2025-07-11 12:06:24 +0100

    TEST: Allow tests to run on previous release

--- tests/test_experiment/test_spectrocal.py
+++ tests/test_experiment/test_spectrocal.py
@@ -1,10 +1,14 @@
-from psychopy.experiment.monitor import BasePhotometerDeviceBackend
+import pytest
 
 
 def test_spectrocalClassDetected():
     """
     Check that the Builder element for SpectroCal is detected (so it appears in Monitor Center)
     """
+    try:
+        from psychopy.experiment.monitor import BasePhotometerDeviceBackend
+    except ModuleNotFoundError:
+        pytest.skip()
     # get all known subclasses of BasePhotometerBackend
     knownClasses = BasePhotometerDeviceBackend.__subclasses__()
     # make sure SpectroCALDeviceBackend is in there
